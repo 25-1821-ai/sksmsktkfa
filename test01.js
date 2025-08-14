68cff9bf61d474398024d1a01b234532a1732aa0
25-1821-ai <25-1821@swjb.hs.kr>
2025-08-14 16:51:53 +0900

    Add files via upload

---
+++ test01.js
@@ -0,0 +1,7 @@
+let reimu02 = 3; // 값을 숫자로 할당, 변수의 자료형 숫자형
+console.log("reimu02 ============================ ");
+console.log(typeof reimu);
+
+let hakurei02 = "3"; // 값을 문자로 할당, 변수의 자료형 문자
+console.log("hakurei02 ============================ ");
+console.log(typeof hakurei);
